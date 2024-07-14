cbb682d7b0de17d614ab48c618b75cf223729796
Pen9uin <sirpen9uin@gmail.com>
2024-07-14 12:16:02 +0300

    done

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,34 @@
 import sum from '../basic';
+import orderByProps from '../app';
 
 test('should sum', () => {
   const result = sum([1, 2, 3]);
 
   expect(result).toBe(6);
+});
+
+test('sorting', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40
+  };
+  expect(orderByProps(obj, ['name', 'level', 'beard'])).toEqual([
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    ]);
+});
+
+test('sorting with an empty array', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  expect(orderByProps(obj, [])).toEqual([
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ]);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,22 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+
+export default function orderByProps(obj, arr) {
+	const objFirst = [];
+	const objSecond = [];
+
+	for (const key in obj) {
+		if (arr.includes(key)) {
+			objFirst.push({key, value: obj[key]});
+		} else {
+			objSecond.push({key, value: obj[key]});
+		}
+	}
+
+	objFirst.sort((a, b) => arr.indexOf(a.key) - arr.indexOf(b.key));
+	objSecond.sort((a, b) => (a.key > b.key ? 1 : -1));
+
+	return [...objFirst, ...objSecond];
+}
